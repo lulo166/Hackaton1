5b9a50f1e29d11aa88b7be4c6f406ff71cb4cf25
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-11-18 20:30:49 +0000

    reorg

---
+++ Ex_4.py
@@ -0,0 +1,10 @@
+'''
+Dans cet exercice, vous allez devoir créer une fonction qui s'appelle somme prennant comme paramètre une liste de nombres 
+et qui retourne la somme de ces nombres. S'il n'y a pas de nombre dans la liste, la fonction devra retourner 0.
+'''
+# écrire votre code ici
+
+
+assert somme([1, 2, 3, 4, 5]) == 15
+assert somme([10, 20, 30, 40, -50]) == 150
+assert somme([]) == 0
