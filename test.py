2c200306afe26cd5c9f4e9196cc262d5a7380426
lulo166 <146172440+lulo166@users.noreply.github.com>
2025-03-05 09:08:29 +0000

    test debut

--- test.py
+++ test.py
@@ -1,9 +1,29 @@
 # Classe, Subclasse,  parcour par largeur, recursif, pile file listechaine, 
 
+recursif
+'''
+Dans cet exercice, vous devez creer une fonction appel
+'''
+# écrire votre code ici
+
+
+assert 
+
+
 
 Classe
 '''
-Dans cet exercice, vous devez creer une classe 
+Dans cet exercice, vous devez creer une classe appelle
+'''
+# écrire votre code ici
+
+
+assert 
+
+
+autre
+'''
+Dans cet exercice, vous devez 
 '''
 # écrire votre code ici
 
